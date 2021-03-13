516643848b7a443884195d6202b2019a59fb0a81
suriyadi <suryadi.surya5@gmail.com>
2021-03-13 23:24:14 +0700

    isPalindrom

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,12 @@
 fun main() {
-    println("Hello world!")
+
+    //check is palindrom
+    println("isPalindrom(aba) : ${isPalindrom("aba")}")
+    println("isPalindrom(adsdas) : ${isPalindrom("adsdas")}")
+    println("isPalindrom(data) : ${isPalindrom("data")}")
+    println("isPalindrom(katak) : ${isPalindrom("katak")}")
+}
+
+fun isPalindrom(input: String) : Boolean{
+    return if(input.isBlank()) false else input == input.reversed()
 }
