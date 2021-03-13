ea05c808b33e89940b8b4a6d0abb1f6d58582321
suriyadi <suryadi.surya5@gmail.com>
2021-03-13 23:54:18 +0700

    prime number

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -16,6 +16,11 @@ fun main() {
     println("fib(10): ${fibonacci(10)}")
     println("fib(20): ${fibonacci(20)}")
     println("fib(100): ${fibonacci(100)}")
+
+    println()
+    println("print prime number")
+    println("prime(10) = ${primeNumber(10)}")
+    println("prime(100) = ${primeNumber(100)}")
 }
 
 fun isPalindrom(input: String): Boolean {
@@ -29,4 +34,29 @@ fun fibonacci(n: Long): Long {
     val result = if (n <= 1) n else (fibonacci(n - 1) + fibonacci(n - 2))
     fiboMaps[n] = result
     return result
+}
+
+fun primeNumber(n: Long): List<Long> {
+    val primeList = mutableListOf<Long>()
+
+    var index = 2L
+    while (true) {
+        if(primeList.size.toLong() == n) break
+
+        if(isPrime(index)){
+            primeList.add(index)
+        }
+        index++
+    }
+    return primeList
+}
+
+fun isPrime(n: Long): Boolean {
+    var isPrime = true
+    for (i in 2 until n) {
+        if (n % i == 0L) {
+            isPrime = false
+        }
+    }
+    return isPrime
 }
